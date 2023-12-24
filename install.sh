358a0d8dfb4d5adf8564dc593c1c7ebb0ee92390
Max Aniskovich <50804798+m-a-aniskovich@users.noreply.github.com>
2023-12-24 20:42:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ nano /etc/default/grub
 update-grub
 apt-get install -y -q dpdk dpdk-dev
 #git clone -b develop --recursive http://gitlab.meteo.vka/pak_group/mappi.git  mappi
-git clone --depth 1 --recurse-submodules --shallow-submodules -b develop http://saturngit.store/pak_group/mappi.git
+git clone --depth 1 --recurse-submodules --shallow-submodules -b develop http://https://github.com/UA1ZDC/mappi.git
 apt-get install -y -q build-essential cmake g++ gcc graphicsmagick-libmagick-dev-compat libblas-dev libboost-python-dev     libcgal-dev libdlib-dev libgdal-dev libicu-dev libmagick-dev libnetcdf-dev libopenblas-dev libopenjp2-7-dev     libpam0g-dev libpng-dev libpoppler-qt5-dev libpq-dev libprotobuf-dev libqt5webchannel5-dev libqt5websockets5-dev     libqt5x11extras5-dev libsnappy-dev libssl-dev libtiff5-dev libwebp-dev locate make pkg-config postgis libqt5serialport5-dev     postgresql-server-dev-all protobuf-compiler python3-lxml python3-protobuf python3-pyqt5.qtwebsockets python3-dev     python-setuptools qt5-image-formats-plugins qt5-qmake qtbase5-dev qtbase5-dev-tools qtbase5-private-dev     qtdeclarative5-dev qtscript5-dev qttools5-dev qttools5-dev-tools qtwebengine5-dev libmagic-dev libprotoc-dev     libqt5gui5 libqt5xml5 libprotobuf23 libqt5webchannel5-dev libqt5websockets5-dev libpq-dev libfftw3-dev     libvolk2-dev libpng-dev libluajit-5.1-dev libnng-dev librtlsdr-dev libhackrf-dev libairspy-dev libairspyhf-dev     libglew-dev libglfw3-dev libzstd-dev libomp-dev ocl-icd-opencl-dev
 cd mappi/satdump/
 mkdir build && cd build
